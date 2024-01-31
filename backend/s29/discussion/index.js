60f7762d6631041c78572895f814bb10dc1a3c64
alvin.estiva <alvin.estiva@tuitt.com>
2024-01-31 19:13:23 +0800

    Add forEach function

--- backend/s29/discussion/index.js
+++ backend/s29/discussion/index.js
@@ -33,3 +33,60 @@ console.log(slicedArrayB);
 let slicedArrayC = countries.slice(-3);
 console.log('Result from slice method:')
 console.log(slicedArrayC);
+
+// toString() - returns an array as string separated by commas
+let stringArray = countries.toString();
+console.log('Result from toString method:');
+console.log(stringArray);
+
+// concat() - combines two arrays and returns the combined result
+let taskArrayA = ['drink html', 'eat javascript'];
+let taskArrayB = ['inhale css', 'breath sass'];
+let taskArrayC = ['get git', 'be node'];
+
+let tasks = taskArrayA.concat(taskArrayB);
+console.log('Result from concat method:')
+console.log(tasks);
+
+// Combining multiple arrays
+let allTasks = taskArrayA.concat(taskArrayB, taskArrayC);
+console.log('Result from concat method:')
+console.log(allTasks);
+
+// Combining arrays with elements
+let combinedTasks = taskArrayA.concat('smell express', 'throw react');
+console.log('Result from concat method:')
+console.log(combinedTasks);
+
+// join() - returns an array as a string separated by specified separator string
+let users = ['John', 'Jane', 'Joe', 'Robert'];
+console.log(users.join());
+console.log(users.join(''));
+console.log(users.join(' - '));
+
+// Iteration methods
+
+// forEach() - similar to a for loop that iterates on each array element
+/*
+	Syntax:
+	  arrayName.forEach(function(indivElement){
+			statement;
+	  })
+*/
+allTasks.forEach(function(task){
+	console.log(task);
+})
+
+// Using forEach with conditional statements
+let filteredTasks = [];
+
+allTasks.forEach(function(task){
+	console.log(task);
+
+	if(task.length > 10){
+		filteredTasks.push(task);
+	}
+})
+
+console.log("Result of filtered tasks:");
+console.log(filteredTasks);
