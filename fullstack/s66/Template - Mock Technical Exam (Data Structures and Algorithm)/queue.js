1d07c22ac221b95a468d96efaeb1bc9df62119c7
Andrei <andrei.amorado@gmail.com>
2024-03-27 19:28:57 +0800

    Added template for mock tech s66

---
+++ fullstack/s66/Template - Mock Technical Exam (Data Structures and Algorithm)/queue.js
@@ -0,0 +1,14 @@
+let collection = [];
+
+// Write the queue functions below.
+// Do not use built-in Array Methods.
+
+module.exports = {
+    collection,
+    print,
+    enqueue,
+    dequeue,
+    front,
+    size,
+    isEmpty
+};
